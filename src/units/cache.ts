7adfdbd712cde34d109de1059b5df1693a435c60
tomershoham10 <tomershoham10@gmail.com>
2024-05-23 10:58:14 +0300

    fix: missdiraction of a file

--- src/units/cache.ts
+++
@@ -1,9 +0,0 @@
-import redis from '../redisClient.js';
-
-export const getFromCache = async (key: string): Promise<string | null> => {
-    return await redis.get(key);
-};
-
-export const setToCache = async (key: string, value: string, expireTime: number = 3600): Promise<void> => {
-    await redis.set(key, value, 'EX', expireTime); // Default expire time is 1 hour
-};
