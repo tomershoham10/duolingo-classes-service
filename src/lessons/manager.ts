ce4615b6e152d7cda7af099706005d575f27b287
tomershoham10 <tomershoham10@gmail.com>
2024-05-23 15:16:23 +0300

    feat: cached Lessons

--- src/lessons/manager.ts
+++ src/lessons/manager.ts
@@ -1,3 +1,4 @@
+import { getFromCache, resetNamespaceCache, setToCache } from "../utils/cache.js";
 import LessonsRepository from "./repository.js";
 
 export default class LessonsManager {
@@ -5,6 +6,8 @@ export default class LessonsManager {
         lesson: Partial<LessonsType>): Promise<LessonsType> {
         try {
             const response = await LessonsRepository.createLesson(lesson);
+            await setToCache('lessons', response._id, JSON.stringify(response), 3600);
+            await resetNamespaceCache('getAllLessons', 'allLessons');
             return response;
         }
         catch (error: any) {
@@ -15,7 +18,15 @@ export default class LessonsManager {
 
     static async getLessonById(lessonId: string): Promise<LessonsType | null> {
         try {
+            const cachedLesson = await getFromCache('lessons', lessonId);
+            if (cachedLesson) {
+                console.log("Cache hit: lessons manager - getLessonById", lessonId);
+                return JSON.parse(cachedLesson); // Parse cached JSON data
+            }
+
             const lesson = await LessonsRepository.getLessonById(lessonId);
+            lesson !== null ? await setToCache('lesson', lessonId, JSON.stringify(lesson), 3600) : null;
+
             console.log("Lessons manager", lesson);
             return lesson;
         }
@@ -27,7 +38,14 @@ export default class LessonsManager {
 
     static async getsExercisesByLessonId(lessonId: string): Promise<FSAType[]> {
         try {
+            const cachedExercises = await getFromCache('getsExercisesByLessonId', lessonId);
+            if (cachedExercises) {
+                console.log("Cache hit: lessons manager - getsExercisesByLessonId", lessonId);
+                return JSON.parse(cachedExercises); // Parse cached JSON data
+            }
+
             const exercises = await LessonsRepository.getsExercisesByLessonId(lessonId);
+            await setToCache('getsExercisesByLessonId', lessonId, JSON.stringify(exercises), 3600)
             console.log("lesson manager getsExercisesByLessonId", exercises);
             return exercises;
         }
@@ -39,7 +57,14 @@ export default class LessonsManager {
 
     static async getsUnsuspendedExercisesByLessonId(lessonId: string): Promise<FSAType[]> {
         try {
+            const cachedExercises = await getFromCache('getsUnsuspendedExercisesByLessonId', lessonId);
+            if (cachedExercises) {
+                console.log("Cache hit: lessons manager - getsUnsuspendedExercisesByLessonId", lessonId);
+                return JSON.parse(cachedExercises); // Parse cached JSON data
+            }
+
             const exercises = await LessonsRepository.getsUnsuspendedExercisesByLessonId(lessonId);
+            await setToCache('getsUnsuspendedExercisesByLessonId', lessonId, JSON.stringify(exercises), 3600)
             console.log("lesson manager getsUnsuspendedExercisesByLessonId", exercises);
             return exercises;
         }
@@ -51,7 +76,13 @@ export default class LessonsManager {
 
     static async getResultsByLessonIdAndUserId(lessonId: string, userId: string): Promise<{ numOfExercises: number, results: ResultType[] } | null | undefined> {
         try {
+            const cachedResults = await getFromCache('getResultsByLessonIdAndUserId', lessonId + userId);
+            if (cachedResults) {
+                console.log("Cache hit: lessons manager - getResultsByLessonIdAndUserId", lessonId + userId);
+                return JSON.parse(cachedResults); // Parse cached JSON data
+            }
             const results = await LessonsRepository.getResultsByLessonIdAndUserId(lessonId, userId);
+            await setToCache('getResultsByLessonIdAndUserId', lessonId + userId, JSON.stringify(results), 3600)
             console.log("lesson manager getsResultsByLessonId", results);
             return results;
         }
@@ -63,7 +94,16 @@ export default class LessonsManager {
 
     static async getAllLessons(): Promise<LessonsType[]> {
         try {
+            const cachedLessons = await getFromCache('getAllLessons', 'allLessons');
+            if (cachedLessons) {
+                console.log("Cache hit: units manager - getAllUnits", cachedLessons);
+                return JSON.parse(cachedLessons); // Parse cached JSON data
+            }
+
             const lessons = await LessonsRepository.getAllLessons();
+
+            await setToCache('getAllLessons', 'allLessons', JSON.stringify(lessons), 3600);
+
             return lessons;
         } catch (error: any) {
             console.error('Manager Error [getAllLessons]:', error.message);
@@ -80,6 +120,9 @@ export default class LessonsManager {
                 lessonId,
                 filedsToUpdate
             );
+            await setToCache('lessons', lessonId, JSON.stringify(updatedLesson), 3600);
+            await resetNamespaceCache('getAllLessons', 'allLessons');
+
             return updatedLesson;
         } catch (error: any) {
             console.error('Manager Error [updateLesson]:', error.message);
@@ -90,6 +133,9 @@ export default class LessonsManager {
     static async deleteLesson(lessonId: string): Promise<LessonsType | null> {
         try {
             const status = await LessonsRepository.deleteLesson(lessonId);
+            status ? await resetNamespaceCache('lessons', lessonId) : null;
+            await resetNamespaceCache('getAllLessons', 'allLessons');
+
             return status;
         } catch (error: any) {
             console.error('Manager Error [deleteLesson]:', error.message);
