99e21c2dfaafe99bbfda1bc8fcb7698331bc77ce
tomershoham10 <tomershoham10@gmail.com>
2024-05-23 10:56:38 +0300

    feat: redis caching

---
+++ src/utils/cache.ts
@@ -0,0 +1,9 @@
+import redis from '../redisClient.js';
+
+export const getFromCache = async (namespace: string, key: string): Promise<string | null> => {
+    return await redis.get(`${namespace}:${key}`);
+};
+
+export const setToCache = async (namespace: string, key: string, value: string, expireTime: number = 3600): Promise<void> => {
+    await redis.set(`${namespace}:${key}`, value, 'EX', expireTime); // Default expire time is 1 hour
+};
