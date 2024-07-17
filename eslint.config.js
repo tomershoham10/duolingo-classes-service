e877df225956723c7d2bb9862bc7899c42f034c4
tomershoham10 <tomershoham10@gmail.com>
2024-07-17 09:08:18 +0300

    feat: lint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+// eslint.config.js
+import { ESLint } from "eslint";
+
+const config = new ESLint({
+  baseConfig: {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "airbnb-base",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+      // Add custom rules if needed
+    },
+  },
+  overrideConfigFile: true,
+});
+
+export default config;
